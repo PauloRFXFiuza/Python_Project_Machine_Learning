de192a0fd1ea7572923e0d33aecbd2ab7fa1a588
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-29 10:36:08 -0300

    v.1.5

--- projectMachineLearning.py
+++ projectMachineLearning.py
@@ -2,5 +2,19 @@
 Projeto Machine Learning'''
 
 import pandas as pd, matplotlib.pyplot as plt, seaborn as sb 
-from pandas_profiling import ProfileReport as pr
+'''from pandas_profiling import ProfileReport as pr'''
 
+pd.options.display.float_format = '{:,.2f}'.format
+'''%matplotlib inline'''
+
+'''Show Data Base
+Mostrar a base de dados'''
+
+base = pd.read_csv("melb_data.csv")
+print(base)
+
+'''Remove columns with high cardinality
+Retirar colunas que tiverem uma alta cardinalidade'''
+
+base = base.drop(["Suburb", "Address", "SellerG", "Date"], axis=1)
+print(base)
