74867d7c6569290576a62f470074322c4e7dea04
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-29 10:24:56 -0300

    v.1.4

---
+++ projectMachineLearning.py
@@ -0,0 +1,6 @@
+'''Machine Learning Project - By Paulo Fiuza- 12/29/2024
+Projeto Machine Learning'''
+
+import pandas as pd, matplotlib.pyplot as plt, seaborn as sb 
+from pandas_profiling import ProfileReport as pr
+
